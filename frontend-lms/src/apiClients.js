e45893bdbfa719e7a214187589cf4839d663de8d
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2024-05-17 23:10:55 +0300

    docker

--- frontend-lms/src/apiClients.js
+++ frontend-lms/src/apiClients.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 const userMicroSvc = axios.create({
-  baseURL: 'http://localhost:8085',
+  baseURL: 'http://usersvc:8085',
 });
 
 const courseMicroSvc = axios.create({
-  baseURL: 'http://localhost:8080/CourseMgmtMs-1.0/api',
+  baseURL: 'http://coursesvc:8080/CourseMgmtMs-1.0/api',
 });
 
 const notifyMicroSvc = axios.create({
-    baseURL: 'http://localhost:8090',
+    baseURL: 'http://notifysvc:8090',
   });
 
 export { userMicroSvc, courseMicroSvc, notifyMicroSvc };
